0624e407c0d514476cadf5ecc5500fbe865025c0
Summer <csy13808796047@gmail.com>
2020-02-20 19:40:18 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
